68408485af87aa0c54cdbe9478dd0ad46bc36ca6
Aswin <techno.aswin@gmail.com>
2010-04-20 00:35:55 +0530

    vyanjana identification

--- src/mizhi.py
+++ src/mizhi.py
@@ -1,6 +1,19 @@
+#===============================================================================
+# This is the starting phase of Mizhi OCR. This code reads the filename from the user.
+# Then the image is divided into lines and is stored in the folder "../temp/lines".
+# Each line is further split into characters and is stored in the folder "../temp/chars"
+# Then the signature of all the individual characters is found out.
+# Classification stage is still to be done.
+#===============================================================================
+
+
 from PIL import Image
 import os
+import sys
+from classify2 import *
  
+#This functions accepts a filename. Convert the image into binary and store it in
+#../temp/
 def binary(filename):
     a=Image.open(filename)
     pix=a.load()
@@ -21,10 +34,11 @@ def purge(l):
             i=i+1
         else:
             b.append((l[i],l[i+1]))
-            #b.append(l[i+1])
             i=i+1
     return b
 
+#Divides the converted binary image into lines and stores it in the location 
+#../temp/lines/
 def horizontal():
     a=Image.open("../temp/binary.png")
     pix=a.load()
@@ -46,6 +60,9 @@ def horizontal():
         q.save("../temp/lines/"+str(j)+".png")
         j=j+1
 
+#Opens each file in "../temp/lines/" and divide it into individual characters.
+#The images are stored in ../temp/chars/
+#This function returns the last file name.
 def vertical(filename,newname):
     a = Image.open("../temp/lines/"+filename)
     pix = a.load()
@@ -62,13 +79,14 @@ def vertical(filename,newname):
         
     newlines = purge(lines)
 
-#    j=0
     for i in newlines:
         q=a.crop((i[0],0,i[1],a.size[1]))
         q.save("../temp/chars/"+str(newname)+".png")
         newname=newname+1
-    return newname  
+    return newname  #This is a crude way to ensure that the images of characters are
+                    #named in sequence. It would be better if a static variable is used here
 
+# opens each file in ../temp/chars and crops it to a fitting box
 def horizontal2(filename):
     a=Image.open("../temp/chars/"+filename)
     pix=a.load()
@@ -83,20 +101,22 @@ def horizontal2(filename):
             lines.append(i)
         
     newlines = purge(lines)
-#    print newlines
     j=0
     for i in newlines:
         q=a.crop((0, i[0], a.size[0], i[1]))
         q.save("../temp/chars/"+filename)
         j=j+1
+        
+#Helper function to horizontal2
 def horizontalagain():
     for i in os.listdir("../temp/chars/"):
         horizontal2(i)
 
-def classify(filename):
+#Resizes each image in ../temp/chars to 20X20 and finds out the signature of each.
+#Then based on the signature the corresponding character is found
+def extract_feature(filename):
     a = Image.open("../temp/chars/"+filename)
     reduceto20 = a.resize((20,20),Image.NEAREST)
-#for a single character.when thereis a bunch we will use a counter
     reduceto20.save("reducedto20.png")
 
     b = Image.open("reducedto20.png")
@@ -108,24 +128,23 @@ def classify(filename):
             if(pix[i,j] == (255,255,255)):
                 whitepixelarray.append((i,j))
                 signature[j/5 + (i/5)*4] = signature[j/5 + (i/5)*4] + 1
-
-#    print "Locations of white pixels"
-#    print whitepixelarray
-    print signature
+    print classify(signature) 
     
         
 if __name__=="__main__":
-    binary("vya.png")
+    if len(sys.argv)==1:
+        name=raw_input("Enter filename : ")
+        binary(name)
+    else:
+        binary(sys.argv[1])
     horizontal()
     j=0
-#    print os.listdir("../temp/lines/")
     filelist=os.listdir("../temp/lines/")
     filelist.sort()
-#    print filelist
     for i in filelist:
         j=vertical(i,j)
         j=j+1
     horizontalagain()
     for i in os.listdir("../temp/chars/"):
-        classify(i)
+        extract_feature(i)
     print "done"
